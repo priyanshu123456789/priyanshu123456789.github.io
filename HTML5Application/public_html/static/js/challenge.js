3910e9b4638c0e304808ea748ac60912085d64ac
priyanshu123456789 <52622977+priyanshu123456789@users.noreply.github.com>
2020-03-28 08:56:29 +0530

    Update challenge.js

--- HTML5Application/public_html/static/js/challenge.js
+++ HTML5Application/public_html/static/js/challenge.js
@@ -1,6 +1,9 @@
 function ageindays(){
  var birthyear=prompt('whats your year of birth?');
- var age=(2020-birthyear)*365;
+var today = new Date();
+var year=parseInt(today.getFullYear());
+var age=(year/4)%10;
+age=age +(year-birthyear)*365;
  var h1= document.createElement('h1');
  var textAnswer= document.createTextNode('you are'+ age+"old"); 
  h1.setAttribute('id','ageindays');
